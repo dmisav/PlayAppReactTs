c6c2fe02ded6be71e69991d79fb083659331b998
Dmitriy Savchenko <savchenko.di@gmail.com>
2023-11-10 18:59:59 -0500

    added new compoents
    added new hintbox

--- react-ts-playapp/src/components/PlayGoalList.tsx
+++ react-ts-playapp/src/components/PlayGoalList.tsx
@@ -1,12 +1,33 @@
 import PlayGoal from "./PlayGoal.tsx";
 import { type PlayGoal as PGoal } from  "../App.tsx"
+import InfoBox from "./InfoBox.tsx";
+import {ReactNode} from "react";
 
 type PlayGoalListProps ={
     goals: PGoal [];
     onDeleteGoal: (id: number) => void; 
 }
 export default function PlayGoalList({goals, onDeleteGoal}: PlayGoalListProps){
+    if (goals.length === 0) {
+        return (
+            <InfoBox mode="hint">
+                You have no goals yet
+            </InfoBox>);
+    }
+    
+let warningBox: ReactNode; 
+    
+if (goals.length >4){
+    warningBox = (
+        <InfoBox mode="warning" severity="high">
+            Dont' put too much goals
+        </InfoBox>
+    );
+}
+    
 return (
+    <>
+        {warningBox}
     <ul>
         { goals.map((goal) => (
             <li key={goal.id}>
@@ -16,5 +37,6 @@ return (
             </li>
         ))}
     </ul>
+    </>
     );
 }
